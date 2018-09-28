c863e13c6fe2f2ebe66028960edeba4c1fc499db
dimazombie <sushkevichd@gmail.com>
2018-09-28 17:59:48 +0300

    revert changes

--- src/main/java/com/netcracker/edu/sat/stub/Launcher.java
+++ src/main/java/com/netcracker/edu/sat/stub/Launcher.java
@@ -7,14 +7,14 @@ import java.util.Locale;
 public class Launcher {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.ENGLISH);
-        Server server = new Server(8889);
+        Server server = new Server(8888);
         WebAppContext context = new WebAppContext("src/main/webapp", "/");
 
         if (System.getProperty("os.name").toLowerCase().contains("windows")) {
             // Fix for Windows, so Jetty doesn't lock files
             context.getInitParams().put("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false");
         }
-        //test
+
         server.setHandler(context);
         server.start();
     }
